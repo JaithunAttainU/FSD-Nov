1fd9c8577a11b70d19e1c7d63a9431fd4c8f7df8
jmahirakz <jmahirakz@paypal.com>
2023-03-31 23:05:17 +0530

    DOM Manipulation

---
+++ Front-End/Js/Day-1-DOM/script.js
@@ -0,0 +1,50 @@
+console.log("Script File is linked")
+
+// window.document.body.style.backgroundColor = 'green'
+
+const clickBtnElement = window.document.getElementById('click-me-btn')
+const bodyElement = document.body
+// console.log(clickBtnElement)
+
+
+//.addEventListener('eventName', function to execute when the event occurs)
+clickBtnElement.addEventListener('click', clickBtnEventListener)
+
+clickBtnElement.addEventListener('mouseenter', function () {
+  console.log("MouseEnter Event trigerred")
+})
+
+clickBtnElement.addEventListener('mouseleave', function () {
+  console.log("MouseLeave Event trigerred")
+})
+
+function clickBtnEventListener() {
+  // console.log("Button is clicked!")
+
+  // 1. Create Div Element - <div>Hello</div>
+  const divElement = document.createElement('div'); //<div></div>
+  divElement.innerText = 'Hello' //<div>Hello</div>
+
+  //2. Insert the created Element inside dom
+  bodyElement.append(divElement) //<body><div>Hello</div></body>
+
+}
+
+const bird = {
+  size: 'small',
+};
+
+
+
+const mouse = {
+  name: 'Mickey',
+  small: true,
+};
+
+mouse.name = 'Mickey'
+mouse["name"] = "Mickey"
+
+mouse.bird.size is not valid
+mouse[bird.size] is not valid - correct
+
+mouse[bird["size"]] - mouse["small"] - correct
