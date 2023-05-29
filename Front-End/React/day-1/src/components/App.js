4f4ca5769e99b07e0d1ef9b76cb164681ae09e0c
jmahirakz <jmahirakz@paypal.com>
2023-05-29 23:06:44 +0530

    React Introduction

---
+++ Front-End/React/day-1/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import YoutubeCard from "./YoutubeCard";
+
+//functional Component
+function App() {
+  return (
+    <>
+      <h1>App Component</h1>
+      <YoutubeCard />
+      <YoutubeCard />
+      <YoutubeCard />
+    </>
+  )
+}
+
+export default App;
---
+++ Front-End/React/day-1/src/components/YoutubeCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+function YoutubeCard() {
+  return (
+    <>
+      <h1>YoutubeCard Component</h1>
+      {/* <Thumbnail />
+      <Details /> */}
+    </>
+  )
+}
+
+export default YoutubeCard;
